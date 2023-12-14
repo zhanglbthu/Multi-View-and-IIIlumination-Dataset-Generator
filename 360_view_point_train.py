2fce9064deb8284d2e4f976811f46d4c35c6fbfc
root <zhanglb21@mails.tsinghua.edu.cn>
2023-12-14 10:52:42 +0800

    direction_light

--- 360_view_point_train.py
+++ 360_view_point_train.py
@@ -8,24 +8,40 @@ import mathutils
 import numpy as np
          
 DEBUG = False
-            
-VIEWS = 100
+
+H_CAMERA_VIEWS = 2
+h_camera_stepsize = 360.0 / H_CAMERA_VIEWS
+V_CAMERA_VIEWS = 2
+v_camera_stepsize = 90.0 / V_CAMERA_VIEWS
+
+H_LIGHT_VIEWS = 2
+h_light_stepsize = 360.0 / H_LIGHT_VIEWS
+V_LIGHT_VIEWS = 2
+v_light_stepsize = 90.0 / V_LIGHT_VIEWS
+
+CAMERA_VIEWS = H_CAMERA_VIEWS * V_CAMERA_VIEWS
+LIGHT_VIEWS = H_LIGHT_VIEWS * V_LIGHT_VIEWS
+TOTAL_VIEWS = CAMERA_VIEWS * LIGHT_VIEWS
+
 RESOLUTION = 800
 # RESULTS_PATH = 'train'
 DEPTH_SCALE = 1.4
-COLOR_DEPTH = 32
+COLOR_DEPTH = 32 # change
 FORMAT = 'OPEN_EXR'
-RANDOM_VIEWS = True
-FIX_VIEWS = False
+
 UPPER_VIEWS = True
 RANDOM_CAMERA = False
+RANDOM_LIGHT = False
+FIX_CAMERA = False
+FIX_LIGHT = False
 
 import sys
-LIGHT_TYPE = sys.argv[-1]
-print("LIGHT_TYPE: {}".format(LIGHT_TYPE))
-RESULTS_PATH = 'train_{}'.format(LIGHT_TYPE)
+RESULTS_PATH = sys.argv[-1]
 
 fp = bpy.path.abspath(f"//{RESULTS_PATH}")
+
+# rm -rf train_YesInteraction
+os.system("rm -rf {}".format(RESULTS_PATH))
 
 def enable_cuda():
     ## this function if borrowed from https://github.com/nytimes/rd-blender-docker/issues/3#issuecomment-618459326
@@ -70,65 +86,6 @@ enable_cuda()
 # Render Optimizations
 bpy.context.scene.render.use_persistent_data = True
 
-# region Turn off interreflections
-bpy.context.scene.cycles.max_bounces = 0
-
-def replace_node_by_diffuse(node_tree, node):
-    if len(node.outputs[0].links) == 0:
-        return
-    links_to_del = []
-    to_sockets = []
-    for i in range(len(node.outputs[0].links)):
-        links_to_del.append(node.outputs[0].links[i])
-        to_sockets.append(node.outputs[0].links[i].to_socket)
-    for link in links_to_del:
-        node_tree.links.remove(link)
-    diffuse_node = node_tree.nodes.new('ShaderNodeBsdfDiffuse')
-    diffuse_node.inputs[0].default_value = node.inputs[0].default_value
-    for to_socket in to_sockets:
-        node_tree.links.new(diffuse_node.outputs[0], to_socket)
-# endregion
-
-#  region Turn off specular effects
-for m in bpy.data.materials:
-    find_bsdf = False
-    for node in m.node_tree.nodes:
-        this_bsdf = True
-        if isinstance(node, bpy.types.ShaderNodeBsdfPrincipled):
-            # node.inputs[7].default_value = 0.0
-            node.inputs[6].default_value = 0.0
-        elif isinstance(node, bpy.types.ShaderNodeEmission):
-            node.inputs[1].default_value = 0.0
-        elif isinstance(node, bpy.types.ShaderNodeBsdfGlossy):
-            pass
-            # replace_node_by_diffuse(m.node_tree, node)
-        elif isinstance(node, bpy.types.ShaderNodeBsdfDiffuse):
-            pass
-        elif isinstance(node, bpy.types.ShaderNodeBsdfGlass):
-            replace_node_by_diffuse(m.node_tree, node)
-        elif isinstance(node, bpy.types.ShaderNodeBsdfAnisotropic):
-            replace_node_by_diffuse(m.node_tree, node)
-        elif isinstance(node, bpy.types.ShaderNodeBsdfTranslucent):
-            replace_node_by_diffuse(m.node_tree, node)
-        elif isinstance(node, bpy.types.ShaderNodeBsdfTransparent):
-            replace_node_by_diffuse(m.node_tree, node)
-        elif isinstance(node, bpy.types.ShaderNodeBsdfVelvet):
-            replace_node_by_diffuse(m.node_tree, node)
-        else:
-            this_bsdf = False
-        if this_bsdf:
-            find_bsdf = True
-    if not find_bsdf:
-        print(f'Cannot find bsdf {m.name}')
-# endregion
-
-# region Remove all external lights
-world_node_tree = bpy.data.worlds['World'].node_tree
-world_links = world_node_tree.nodes['World Output'].inputs[0].links
-if len(world_links) > 0:
-    world_node_tree.links.remove(world_links[0])
-world_node_tree.update_tag()
-
 # Set up rendering of depth map.
 bpy.context.scene.use_nodes = True
 tree = bpy.context.scene.node_tree
@@ -138,7 +95,6 @@ links = tree.links
 #bpy.context.scene.view_layers["RenderLayer"].use_pass_normal = True
 bpy.context.scene.render.image_settings.file_format = str(FORMAT)
 bpy.context.scene.render.image_settings.color_depth = str(COLOR_DEPTH)
-# endregion
 
 if not DEBUG:
     # Create input render layer node.
@@ -166,12 +122,12 @@ if not DEBUG:
 
 # Background
 bpy.context.scene.render.dither_intensity = 0.0
-bpy.context.scene.render.film_transparent = True
+bpy.context.scene.render.film_transparent = False
 
 # Create collection for objects not to render with background
 
 objs = [ob for ob in bpy.context.scene.objects if (ob.type in ('EMPTY') and 'Empty' in ob.name) or 'Sun' in ob.name]
-if FIX_VIEWS:
+if FIX_CAMERA:
     objs = [ob for ob in objs if ob.name != 'Empty']
 bpy.ops.object.delete({"selected_objects": objs})
 
@@ -192,26 +148,23 @@ scene.render.resolution_y = RESOLUTION
 scene.render.resolution_percentage = 100
 
 cam = scene.objects['Camera']
-if not FIX_VIEWS:
-    cam.location = (0, 4.0, 0.5)
-    cam_constraint = cam.constraints.new(type='TRACK_TO')
-    cam_constraint.track_axis = 'TRACK_NEGATIVE_Z'
-    cam_constraint.up_axis = 'UP_Y'
-    b_empty = parent_obj_to_camera('Empty', cam)
-    cam_constraint.target = b_empty
 
-sun_data = bpy.data.lights.new(name='Sun', type=LIGHT_TYPE)
-sun_data.cycles.max_bounces = 0
-if LIGHT_TYPE == 'SUN':
-    sun_data.energy = 1.5
-    sun_data.angle = 0
-elif LIGHT_TYPE == 'POINT':
-    sun_data.energy = 150
-    sun_data.shadow_soft_size = 0
+cam.location = (0, 4.0, 0)
+cam_constraint = cam.constraints.new(type='TRACK_TO')
+cam_constraint.track_axis = 'TRACK_NEGATIVE_Z'
+cam_constraint.up_axis = 'UP_Y'
+b_empty = parent_obj_to_camera('Empty', cam)
+cam_constraint.target = b_empty
+ 
+sun_data = bpy.data.lights.new(name='Sun', type="SUN")
+
+sun_data.energy = 1.5
+sun_data.angle = 0
+
 sun_light = bpy.data.objects.new(name='Sun', object_data=sun_data)
 bpy.context.collection.objects.link(sun_light)
 bpy.context.view_layer.objects.active = sun_light
-sun_light.location = (0, 2.0, 1.0)
+sun_light.location = (0, 2, 0)
 p_empty = parent_obj_to_camera('Empty_light', sun_light)
 light_constraint = sun_light.constraints.new(type='TRACK_TO')
 light_constraint.track_axis = 'TRACK_NEGATIVE_Z'
@@ -223,8 +176,6 @@ scene.render.image_settings.file_format = 'PNG'  # set output format to .png
 
 from math import radians
 
-
-stepsize = 360.0 / VIEWS
 rotation_mode = 'XYZ'
 
 if not DEBUG:
@@ -233,32 +184,57 @@ if not DEBUG:
 
 out_data['frames'] = []
 
+def computer_cam_rotation(i):
+    if RANDOM_CAMERA:
+        if UPPER_VIEWS:
+            rot = np.random.uniform(0, 1, size=3) * (1,0,2*np.pi)
+            rot[0] = np.abs(np.arccos(1 - 2 * rot[0]) - np.pi/2)
+            b_empty.rotation_euler = rot
+        else:
+            b_empty.rotation_euler = np.random.uniform(0, 2*np.pi, size=3)
+    else:
+        if i != 0:
+            if i % H_CAMERA_VIEWS == 0:
+                b_empty.rotation_euler[0] += radians(v_camera_stepsize)
+                b_empty.rotation_euler[2] = 0
+            else:
+                b_empty.rotation_euler[2] += radians(h_camera_stepsize)
+
+def computer_light_rotation(i):
+    if RANDOM_LIGHT:
+        scene.render.filepath = fp + '/images' + '/r_' + str(i)
+        if UPPER_VIEWS:
+            rot = np.random.uniform(0, 1, size=3) * (1,0,2*np.pi)
+            rot[0] = np.abs(np.arccos(1 - 2 * rot[0]) - np.pi/2)
+            p_empty.rotation_euler = rot
+        else:
+            p_empty.rotation_euler = np.random.uniform(0, 2*np.pi, size=3)
+    else:
+        if i != 0:
+            if i % H_LIGHT_VIEWS == 0:
+                p_empty.rotation_euler[0] += radians(v_light_stepsize)
+                p_empty.rotation_euler[2] = 0
+            else:
+                p_empty.rotation_euler[2] += radians(h_light_stepsize)
+                
 def render_multiview_multilight():
-    for i in range(0, VIEWS):
-        if not FIX_VIEWS:
-            if RANDOM_VIEWS:
-                if UPPER_VIEWS:
-                    rot = np.random.uniform(0, 1, size=3) * (1,0,2*np.pi)
-                    rot[0] = np.abs(np.arccos(1 - 2 * rot[0]) - np.pi/2)
-                    b_empty.rotation_euler = rot
-                else:
-                    b_empty.rotation_euler = np.random.uniform(0, 2*np.pi, size=3)
-                
-        if RANDOM_CAMERA:
-            scene.render.filepath = fp + '/image' + '/r_' + str(i)
-            if UPPER_VIEWS:
-                rot = np.random.uniform(0, 1, size=3) * (1,0,2*np.pi)
-                rot[0] = np.abs(np.arccos(1 - 2 * rot[0]) - np.pi/2)
-                p_empty.rotation_euler = rot
-            else:
-                p_empty.rotation_euler = np.random.uniform(0, 2*np.pi, size=3)
-        else:
-            print("Rotation {}, {}".format((stepsize * i), radians(stepsize * i)))
-            scene.render.filepath = fp + '/image' + '/r_{0:03d}'.format(int(i * stepsize))
+    '''
+    b_empty: camera
+    p_empty: light
+    '''
+    for i in range(0, TOTAL_VIEWS):
+        cam_turn = i % LIGHT_VIEWS == 0
+        if not FIX_CAMERA and cam_turn:
+            computer_cam_rotation(i // LIGHT_VIEWS)
+            p_empty.rotation_euler = np.array([0, 0, 0])
+        elif FIX_CAMERA:
+            b_empty.rotation_euler = np.array([30, 0, 30]) * np.pi / 180.0
+        if not FIX_LIGHT and not cam_turn:
+            computer_light_rotation(i % LIGHT_VIEWS)
+        elif FIX_LIGHT:
+            p_empty.rotation_euler = np.array([30, 0, 30]) * np.pi / 180.0
 
-        # if not DEBUG:
-            # depth_file_output.file_slots[0].path = scene.render.filepath + "_depth_"
-            # normal_file_output.file_slots[0].path = scene.render.filepath + "_normal_"
+        scene.render.filepath = fp + '/images' + '/r_' + str(i)
 
         if DEBUG:
             break
@@ -266,60 +242,47 @@ def render_multiview_multilight():
             bpy.ops.render.render(write_still=True)  # render still
 
         frame_data = {
-            'file_path': os.path.relpath(scene.render.filepath, f'{fp}/..'),
-            'rotation': radians(stepsize),
+            'file_path': 'images' + '/r_' + str(i),
             'transform_matrix': listify_matrix(cam.matrix_world),
             'transform_matrix_sun': listify_matrix(sun_light.matrix_world),
         }
         out_data['frames'].append(frame_data)
 
-        if RANDOM_CAMERA:
-            if UPPER_VIEWS:
-                rot = np.random.uniform(0, 1, size=3) * (1,0,2*np.pi)
-                rot[0] = np.abs(np.arccos(1 - 2 * rot[0]) - np.pi/2)
-                p_empty.rotation_euler = rot
-            else:
-                p_empty.rotation_euler = np.random.uniform(0, 2*np.pi, size=3)
-        else:
-            p_empty.rotation_euler[2] += radians(stepsize)
-
     if not DEBUG:
-        with open(fp + '/' + f'transforms_{RESULTS_PATH}.json', 'w') as out_file:
+        # 获取fp的父文件夹
+        with open(fp + '/' + f'transforms_train.json', 'w') as out_file:
             json.dump(out_data, out_file, indent=4)
 
-def render_multiview_singlelight():
-    for i in range(0, VIEWS):
-        if not FIX_VIEWS:
-            if RANDOM_VIEWS:
-                if UPPER_VIEWS:
-                    rot = np.random.uniform(0, 1, size=3) * (1,0,2*np.pi)
-                    rot[0] = np.abs(np.arccos(1 - 2 * rot[0]) - np.pi/2)
-                    b_empty.rotation_euler = rot
-                else:
-                    b_empty.rotation_euler = np.random.uniform(0, 2*np.pi, size=3)
+# def render_multiview_singlelight():
+    # for i in range(0, VIEWS):
+    #     if not FIX_CAMERA:
+    #         if RANDOM_CAMERA:
+    #             if UPPER_VIEWS:
+    #                 rot = np.random.uniform(0, 1, size=3) * (1,0,2*np.pi)
+    #                 rot[0] = np.abs(np.arccos(1 - 2 * rot[0]) - np.pi/2)
+    #                 b_empty.rotation_euler = rot
+    #             else:
+    #                 b_empty.rotation_euler = np.random.uniform(0, 2*np.pi, size=3)
                 
-        print("Rotation {}, {}".format((stepsize * i), radians(stepsize * i)))
-        scene.render.filepath = fp + '/image' + '/r_{0:03d}'.format(int(i * stepsize))
+    #     print("Rotation {}, {}".format((stepsize * i), radians(stepsize * i)))
+    #     scene.render.filepath = fp + '/images' + '/r_{0:03d}'.format(int(i * stepsize))
 
-        # if not DEBUG:
-            # depth_file_output.file_slots[0].path = scene.render.filepath + "_depth_"
-            # normal_file_output.file_slots[0].path = scene.render.filepath + "_normal_"
+    #     if DEBUG:
+    #         break
+    #     else:
+    #         bpy.ops.render.render(write_still=True)  # render still
 
-        if DEBUG:
-            break
-        else:
-            bpy.ops.render.render(write_still=True)  # render still
+    #     frame_data = {
+    #         'file_path': os.path.relpath(scene.render.filepath, f'{fp}/..'),
+    #         'rotation': radians(stepsize),
+    #         'transform_matrix': listify_matrix(cam.matrix_world),
+    #         'transform_matrix_sun': listify_matrix(sun_light.matrix_world),
+    #     }
+    #     out_data['frames'].append(frame_data)
 
-        frame_data = {
-            'file_path': os.path.relpath(scene.render.filepath, f'{fp}/..'),
-            'rotation': radians(stepsize),
-            'transform_matrix': listify_matrix(cam.matrix_world),
-            'transform_matrix_sun': listify_matrix(sun_light.matrix_world),
-        }
-        out_data['frames'].append(frame_data)
-
-    if not DEBUG:
-        with open(fp + '/' + f'transforms_{RESULTS_PATH}.json', 'w') as out_file:
-            json.dump(out_data, out_file, indent=4)
+    # if not DEBUG:
+    #     with open(fp + '/' + f'transforms_{RESULTS_PATH}.json', 'w') as out_file:
+    #         json.dump(out_data, out_file, indent=4)
             
-render_multiview_singlelight()
+if __name__ == '__main__':
+    render_multiview_multilight()
--- directional_light.sh
+++ directional_light.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-
 blender_path="/root/blender-3.6.5-linux-x64/blender"
-objs=("hotdog_specular_point")
-gpus=(0)
-light_type="SUN"
+objs=(chair_specular_point drums_specular_point ficus_specular_point hotdog_specular_point lego_specular_point materials_specular_point mic_specular_point ship_specular_point)
+gpus=(0 1 2 3 4 5 6 7)
 
+# root_path="/root/autodl-tmp/gaussian-splatting/blender/hotdog_LightOrigin"
+root_path="/root/autodl-tmp/gaussian-splatting/blender"
+image_path="${root_path}/images"
+video_path="${root_path}/video"
+
+tmux_name=directional_light_render
 cur_idx=0
 
 for obj in "${objs[@]}"; do
@@ -13,10 +17,6 @@ for obj in "${objs[@]}"; do
     cd "point_light/${obj}" || exit 1
     export CUDA_VISIBLE_DEVICES=${gpus[cur_idx-1]}
 
-    rm -rf train_"${light_type}"
-    rm -rf normal
-    rm -rf test_one
-    rm -rf floor
-
-    "${blender_path}" --background --factory-startup main.blend --python ../../360_view_point_train.py -- "${light_type}"
+    "${blender_path}" --background --factory-startup main.blend --python ../../360_view_point_train.py -- "${root_path}"
+    python ../../image2video.py "${image_path}" "${video_path}" 
 done
